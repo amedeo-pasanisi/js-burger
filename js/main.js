0c1a615197a1034f94b8dd6d04fb38a5ecd83085
amedeo <amedeopasanisi97@gmail.com>
2021-09-04 19:29:58 +0200

    check/uncheck js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 // Il programma dovrà consentire 
 // di calcolare il prezzo del panino selezionando o deselezionando gli ingredienti proposti.
+
+const check = document.getElementsByClassName("fake-checkbox");
+
+for (let i=0; i<check.length; i++) {
+    check[i].addEventListener("click",
+    function () {
+        check[i].previousElementSibling.checked = !check[i].previousElementSibling.checked
+    })
+}
